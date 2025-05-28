2ad18c29483c4410f4802ad0430148a13339b1e0
amirrasooli69 <amirrasooli69@yahoo.com>
2025-05-28 20:45:22 +0330

    firest commit

---
+++ train/amir-rasouli-w10/script.js
@@ -0,0 +1,12 @@
+const createStepButton = document.getElementById("create-step")
+const nextStep = document.getElementById("next-button")
+const prevStep = document.getElementById("prev-button")
+
+let step = null;
+const createStepHanler = ()=>{
+    
+}
+
+createStepButton.addEventListener("click", createStepHanler)
+nextStep.addEventListener("click", nextStepHandler)
+prevStep.addEventListener("click", prevStepHandler)
